91622fe5220bee9829f0ea0741beb8dcccce559b
ravibatchala <batchalaraviteja@gmail.com>
2021-08-01 13:43:37 +0530

    done the StatusLine function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,57 @@
+import { useEffect, useState } from "react";
 import "../styles/App.scss";
+import StatusLine from "./StatusLine";
 
 const App = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        console.log("using effect");
+    }, []);
+
+    const addEmptyTask = (status) => {};
+
+    const addTask = (taskToAdd) => {};
+
+    const deleteTask = (taskId) => {};
+
+    const moveTask = (id, newStatus) => {};
+
+    const saveTasksToLocalStorage = (tasks) => {};
+
+    const loadTasksToLocalStorage = () => {};
+
     return (
         <div className="App">
             <h1>Task Management</h1>
+            <main>
+                <section>
+                    <StatusLine
+                        task={tasks}
+                        addEmptyTask={addEmptyTask}
+                        addTask={addTask}
+                        deleteTask={deleteTask}
+                        moveTask={moveTask}
+                        status="BackLog"
+                    />
+                    <StatusLine
+                        task={tasks}
+                        addEmptyTask={addEmptyTask}
+                        addTask={addTask}
+                        deleteTask={deleteTask}
+                        moveTask={moveTask}
+                        status="In Progress"
+                    />
+                    <StatusLine
+                        task={tasks}
+                        addEmptyTask={addEmptyTask}
+                        addTask={addTask}
+                        deleteTask={deleteTask}
+                        moveTask={moveTask}
+                        status="Done"
+                    />
+                </section>
+            </main>
         </div>
     );
 };
--- src/components/StatusLine.js
+++ src/components/StatusLine.js
@@ -1,7 +1,49 @@
 import "../styles/statusLine.scss";
+import Task from "./Task";
 
-const StatusLine = () => {
-    return <div className="statusLine">S</div>;
+const StatusLine = ({
+    status,
+    task,
+    addEmptyTask,
+    addTask,
+    deleteTask,
+    moveTask,
+}) => {
+    let taskList, tasksForStatus;
+
+    const handleAddEmpty = () => {
+        addEmptyTask(status);
+    };
+
+    if (task) {
+        tasksForStatus = task.filter((task) => {
+            return task.status === status;
+        });
+    }
+
+    if (tasksForStatus) {
+        taskList = tasksForStatus.map((task) => {
+            return (
+                <Task
+                    addTask={(task) => addTask(task)}
+                    deleteTask={(id) => deleteTask(id)}
+                    moveTask={(id, status) => moveTask(id, status)}
+                    key={task.id}
+                    task={task}
+                />
+            );
+        });
+    }
+
+    return (
+        <div className="statusLine">
+            <h3>{status}</h3>
+            {taskList}
+            <button onClick={handleAddEmpty} className="button addTask">
+                +
+            </button>
+        </div>
+    );
 };
 
 export default StatusLine;
