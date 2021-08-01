6ae4c4737e1403b7803adea1fa5ce97b3009675c
ravibatchala <batchalaraviteja@gmail.com>
2021-08-01 13:07:38 +0530

    created the styles for the app

---
+++ src/components/StatusLine.js
@@ -0,0 +1,7 @@
+import "../styles/statusLine.scss";
+
+const StatusLine = () => {
+    return <div className="statusLine">S</div>;
+};
+
+export default StatusLine;
---
+++ src/components/Task.js
@@ -0,0 +1,7 @@
+import "../styles/task.scss";
+
+const Task = () => {
+    return <div className="task">t</div>;
+};
+
+export default Task;
